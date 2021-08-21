051fefd16c934bd57296a8ba145136828eb33856
farzanapomy <farzanapomy56@gmail.com>
2021-08-22 04:56:24 +0600

    updated

--- js/style.js
+++ js/style.js
@@ -3,19 +3,18 @@ const memoryPrice = document.getElementById('extraMemory-price');
 const storagePrice = document.getElementById('extraStorage-price');
 const delivaryPrice = document.getElementById('delivery-charge');
 const totalPrice = document.getElementById('total-charge');
-const discount= document.getElementById('discount-price');
-
+const discount = document.getElementById('discount-price');
 
 
 document.getElementById('low-memory').addEventListener('click', function () {
     memoryPrice.innerText = '0';
-  
+
 
     updatePrice()
 })
 document.getElementById('high-memory').addEventListener('click', function () {
     memoryPrice.innerText = '180';
-   
+
     updatePrice()
 })
 
@@ -23,32 +22,31 @@ document.getElementById('high-memory').addEventListener('click', function () {
 
 document.getElementById('low-storage').addEventListener('click', function () {
     storagePrice.innerText = '0';
-    
+
     updatePrice()
 })
 document.getElementById('medium-storage').addEventListener('click', function () {
     storagePrice.innerText = '100';
-    
+
     updatePrice()
 })
 document.getElementById('high-storage').addEventListener('click', function () {
     storagePrice.innerText = '180';
-   
+
     updatePrice()
 })
 
 // delivary Charge
 document.getElementById('free-delivery').addEventListener('click', function () {
     delivaryPrice.innerText = '0';
-    
+
     updatePrice()
 })
 document.getElementById('cost-delivery').addEventListener('click', function () {
     delivaryPrice.innerText = '20';
-    
+
     updatePrice()
 })
-
 
 
 // total price 
@@ -57,30 +55,41 @@ function updatePrice() {
     const storageCharge = parseInt(storagePrice.innerText);
     const delivaryCharge = parseInt(delivaryPrice.innerText);
     const actualCharge = parseInt(actualPrice.innerText);
-    const total = memoryCharge + storageCharge + delivaryCharge+actualCharge;
+    const total = memoryCharge + storageCharge + delivaryCharge + actualCharge;
     totalPrice.innerText = total;
-    discount.innerText=total;
+    discount.innerText = total;
 }
 
-// discount price 
-discount.innerText=total;
+
+// bonus part 
+// promo price 
+const promo = document.getElementById('promo-text');
+
+document.getElementById('promo-apply').addEventListener('click', function () {
+    
+    promoPrice()
+    promo.value = '';
+})
+
+function promoPrice() {
+    const promoText = promo.value;
+    const previousPrice = parseFloat(totalPrice.innerText) * .2;
+    const discountPrice = parseFloat(totalPrice.innerText) - previousPrice;
+
+    if (promo.value == 'stevekaku') {
+        discount.innerText = discountPrice;
+    }
+}
+   
 
 
 
 
 
+// after  discount price 
+// discount.innerText = total;
 
 
-// function promoPrice(promo) {
-//     if(promo=='stevekaku'){
-//         const discount = totalPrice-(totalPrice/20);
-//     }
-//     return discount;
-// }
-
-// document.getElementById('promo-value').addEventListener('click',function() {
-//      promoPrice();   
-// })
 
 
 
