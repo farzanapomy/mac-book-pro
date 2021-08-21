2fc9f32dcff8a2c0f20d170f835340f0e5c8ebc2
farzanapomy <farzanapomy56@gmail.com>
2021-08-22 02:30:17 +0600

    updated

--- js/style.js
+++ js/style.js
@@ -1,144 +1,86 @@
- 
+const actualPrice = document.getElementById('actual-price');
+const memoryPrice = document.getElementById('extraMemory-price');
+const storagePrice = document.getElementById('extraStorage-price');
+const delivaryPrice = document.getElementById('delivery-charge');
+const totalPrice = document.getElementById('total-charge');
+const discount= document.getElementById('discount-price');
 
 
 
+document.getElementById('low-memory').addEventListener('click', function () {
+    memoryPrice.innerText = '0';
+  
 
-
-
-document.getElementById('memory-low').addEventListener('click', function () {
-    const memorycharge = document.getElementById('extraMemory-price');
-    const memorychargeText = parseFloat(memorycharge.innerText);
-    memorycharge.innerText = '0'; 
-
-    const totalCharge = document.getElementById('total-charge');
-    const totalChargeText = totalCharge.innerText;
-    totalCharge.innerText = '1299';
-
-    const totalPrice = document.getElementById('total-price');
-    const totalPriceText = totalPrice.innerText;
-    totalPrice.innerText = '1299';
-
-}) 
- 
-document.getElementById('memory-high').addEventListener('click', function () {
-    const memorycharge = document.getElementById('extraMemory-price');
-    const memorychargeText = memorycharge.innerText;
-    memorycharge.innerHTML = '180';
-    const memorychargenumber = parseFloat(memorycharge.innerText);
-
-    const totalCharge = document.getElementById('total-charge');
-    const totalChargeText = totalCharge.innerText;
-    let totalChargeNumber = parseFloat(totalCharge.innerText)
-    totalCharge.innerText = memorychargenumber + totalChargeNumber;
-
-    const totalPrice = document.getElementById('total-price');
-    const totalPriceText = totalPrice.innerText;
-    let totalPriceNumber = parseFloat(totalPrice.innerText)
-    totalPrice.innerText = memorychargenumber + totalChargeNumber;
+    updatePrice()
+})
+document.getElementById('high-memory').addEventListener('click', function () {
+    memoryPrice.innerText = '180';
    
+    updatePrice()
 })
 
 // storage price 
 
 document.getElementById('low-storage').addEventListener('click', function () {
-    const storageCharge = document.getElementById('extraStorage-price');
-    const storageChargeNumber = parseFloat(storageCharge.innerText);
-    storageCharge.innerText = '0';
-
-    const totalCharge = document.getElementById('total-charge');
-    const totalChargeText = totalCharge.innerText;
-    totalCharge.innerText = '1299';
-
-    const totalPrice = document.getElementById('total-price');
-    const totalPriceText = totalPrice.innerText;
-    totalPrice.innerText = '1299';
-})
-
-document.getElementById('medium-storage').addEventListener('click', function () {
-    const storageCharge = document.getElementById('extraStorage-price');
-    const storageChargeText = storageCharge.innerText;
-    storageCharge.innerText = '100';
-    const storageChargeNumber = parseFloat(storageCharge.innerText);
-
-    const totalCharge = document.getElementById('total-charge');
-    const totalChargeText = totalCharge.innerText;
-    let totalChargeNumber = parseFloat(totalCharge.innerText)
-    totalCharge.innerText = storageChargeNumber + totalChargeNumber;
-
-    const totalPrice = document.getElementById('total-price');
-    const totalPriceText = totalPrice.innerText;
-    let totalPriceNumber = parseFloat(totalPrice.innerText)
-    totalPrice.innerText = storageChargeNumber + totalChargeNumber;
-})
-
-document.getElementById('high-storage').addEventListener('click', function () {
-    const storageCharge = document.getElementById('extraStorage-price');
-    const storageChargeText = storageCharge.innerText;
-    storageCharge.innerText = '180';
-    const storageChargeNumber = parseFloat(storageCharge.innerText);
-
-    const totalCharge = document.getElementById('total-charge');
-    const totalChargeText = totalCharge.innerText;
-    let totalChargeNumber = parseFloat(totalCharge.innerText)
-    totalCharge.innerText = storageChargeNumber + totalChargeNumber;
-
-    const totalPrice = document.getElementById('total-price');
-    const totalPriceText = totalPrice.innerText;
-    let totalPriceNumber = parseFloat(totalPrice.innerText)
-    totalPrice.innerText = storageChargeNumber + totalChargeNumber;
-})
-
-
-// delivery charge 
-
-
-document.getElementById('free-delivery').addEventListener('click', function () {
-
-    const delivaryCharge = document.getElementById('delivery-charge')
-    const delivaryChargeText = delivaryCharge.innerText;
-    delivaryCharge.innerText = '0';
-    const delivaryChargenumber = parseFloat(delivaryCharge.innerText);
-
-    const totalCharge = document.getElementById('total-charge');
-    const totalChargeText = totalCharge.innerText;
-    totalCharge.innerText = '1299';
-
-    const totalPrice = document.getElementById('total-price');
-    const totalPriceText = totalPrice.innerText;
-    totalPrice.innerText = '1299';
-})
-
-document.getElementById('cost-delivery').addEventListener('click', function () {
+    storagePrice.innerText = '0';
     
-    const delivaryCharge = document.getElementById('delivery-charge')
-    const delivaryChargeText = delivaryCharge.innerText;
-    delivaryCharge.innerText = '20';
-    const delivaryChargenumber = parseFloat(delivaryCharge.innerText);
+    updatePrice()
+})
+document.getElementById('medium-storage').addEventListener('click', function () {
+    storagePrice.innerText = '100';
+    
+    updatePrice()
+})
+document.getElementById('high-storage').addEventListener('click', function () {
+    storagePrice.innerText = '180';
+   
+    updatePrice()
+})
 
-    const totalCharge = document.getElementById('total-charge');
-    const totalChargeText = totalCharge.innerText;
-    let totalChargeNumber = parseFloat(totalCharge.innerText)
-    totalCharge.innerText = delivaryChargenumber + totalChargeNumber;
-
-    const totalPrice = document.getElementById('total-price');
-    const totalPriceText = totalPrice.innerText;
-    let totalPriceNumber = parseFloat(totalPrice.innerText)
-    totalPrice.innerText = delivaryChargenumber + totalChargeNumber;
-
+// delivary Charge
+document.getElementById('free-delivery').addEventListener('click', function () {
+    delivaryPrice.innerText = '0';
+    
+    updatePrice()
+})
+document.getElementById('cost-delivery').addEventListener('click', function () {
+    delivaryPrice.innerText = '20';
+    
+    updatePrice()
 })
 
 
 
-function promoPrice(promo) {
-    if(promo=='stevekaku'){
-        const discount = totalPrice-(totalPrice/20);
-    }
-    return discount;
+// total price 
+function updatePrice() {
+    const memoryCharge = parseInt(memoryPrice.innerText);
+    const storageCharge = parseInt(storagePrice.innerText);
+    const delivaryCharge = parseInt(delivaryPrice.innerText);
+    const actualCharge = parseInt(actualPrice.innerText);
+    const total = memoryCharge + storageCharge + delivaryCharge+actualCharge;
+    totalPrice.innerText = total;
+    discount.innerText=total;
 }
 
-document.getElementById('promo-value').addEventListener('click',function() {
-     promoPrice();   
-})
+// discount price 
+discount.innerText=total;
+
+
+
+
+
+
+
+// function promoPrice(promo) {
+//     if(promo=='stevekaku'){
+//         const discount = totalPrice-(totalPrice/20);
+//     }
+//     return discount;
+// }
+
+// document.getElementById('promo-value').addEventListener('click',function() {
+//      promoPrice();   
+// })
 
 
 
